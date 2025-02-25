c1e98f10ebcf65f9596b637efc2e8740920b45d5
trindadedev13 <trindadedev13@gmail.com>
2025-02-25 02:31:23 -0300

    fix: Resolve contributors dont appear

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
 async function fetchContributors() {
-  const token = process.env.GITHUB_TOKEN;
   const repo = 'Robok-Engine/Robok-Engine';
 
   try {
-    const response = await fetch(`https://raw.githubusercontent.com/Robok-Engine/Robok-Engine/host/.github/contributors/contributors_github.json`, {
-      headers: {
-        'Authorization': `token ${token}`
-      }
-    });
+    const response = await fetch(`https://api.github.com/repos/${repo}/contributors`);
+
+    if (!response.ok) {
+      throw new Error(`Error:8: ${response.statusText}`);
+    }
+
     const data = await response.json();
 
-    if (response.ok) {
-      const contributorsList = data.map(contributor => {
-        return `<div class="contributor"><a href="https://github.com/${contributor.login}" target="_blank">${contributor.login}</a></div>`;
-      }).join('');
+    const contributorsList = data.map(contributor => {
+      return `<div class="contributor">
+                <a href="${contributor.html_url}" target="_blank">
+                  <img src="${contributor.avatar_url}" alt="${contributor.login}" width="40" height="40">
+                  ${contributor.login}
+                </a>
+              </div>`;
+    }).join('');
 
-      const contributorsContainer = document.createElement('div');
-      contributorsContainer.innerHTML = contributorsList;
-
-      document.getElementById('contributors').appendChild(contributorsContainer);
-    } else {
-      console.error('Failed to fetch contributors:', data);
-    }
+    document.getElementById('contributors').innerHTML = contributorsList;
   } catch (error) {
-    console.error('Error fetching contributors:', error);
+    console.error('Error:24:', error);
   }
 }
 
